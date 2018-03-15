e559730cc9967c6a196dcd862fcf20e18f361a20
Marie Starck <marie.compsci14@gmail.com>
2018-03-14 18:53:33 -0700

    New code

---
+++ src/containers/weather_list.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
+
+
+class WeatherList extends Component {
+	renderWeather(cityData) {
+		const cityName = cityData.city.name
+		const temps = cityData.list.map(weather => weather.main.temp);
+
+		return(
+			<tr key={ cityName }>
+				<td>{ cityName }</td>
+				<td>
+					<Chart data={temps} color="blue" />
+				</td>
+			</tr>
+		);
+	}
+
+	render() {
+		return (
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>City</th>
+						<th>Temperature</th>
+						<th>Pressure</th>
+						<th>Humidity</th>
+					</tr>
+				</thead>
+				<tbody>
+					{ this.props.weather.map(this.renderWeather) }
+				</tbody>
+			</table>
+		);
+	}
+}
+
+function mapStatetoProps({ weather }) {
+	return { weather }; // { weather } = { weather: weather}
+}
+
+export default connect(mapStatetoProps)(WeatherList);
+
