eb32c9b11c8d7111997c922f5d0aaa834f75ae3d
Marie Starck <marie.compsci14@gmail.com>
2018-03-26 11:19:27 -0700

    Pass lat and lon for map + Add averages and units

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 
 class WeatherList extends Component {
 	renderWeather(cityData) {
 		const cityName = cityData.city.name
 		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+		const {lon, lat} = cityData.city.coord;
 
 		return(
 			<tr key={ cityName }>
-				<td>{ cityName }</td>
+				<td><GoogleMap lon={lon} lat={lat}/></td>
 				<td>
-					<Chart data={temps} color="blue" />
+					<Chart data={temps} color="blue" units="K"/>
+				</td>
+				<td>
+					<Chart data={pressures} color="green" units="hPa"/>
+				</td>
+				<td>
+					<Chart data={humidities} color="orange" units="%"/>
 				</td>
 			</tr>
 		);
@@ -24,9 +34,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
